78d9a34125f01e1ec19e638c4b66011322bff6ed
Michael Wanke <Michael.Wanke@forwardadvantage.com>
2015-12-04 01:35:53 -0800

    Add SecureString password support
    Add SecureString password support for Impersonation call

--- SimpleImpersonation/Impersonation.cs
+++ SimpleImpersonation/Impersonation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Security;
 using System.Security.Permissions;
 using System.Security.Principal;
 
@@ -8,26 +9,57 @@ namespace SimpleImpersonation
     [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
     public sealed class Impersonation : IDisposable
     {
-        private readonly SafeTokenHandle _handle;
-        private readonly WindowsImpersonationContext _context;
+        private SafeTokenHandle _handle;
+        private WindowsImpersonationContext _context;
 
         public static Impersonation LogonUser(string domain, string username, string password, LogonType logonType)
         {
             return new Impersonation(domain, username, password, logonType);
         }
 
-        private Impersonation(string domain, string username, string password, LogonType logonType)
+        public static Impersonation LogonUser(string domain, string username, SecureString password, LogonType logonType)
         {
-            IntPtr handle;
-            var ok = NativeMethods.LogonUser(username, domain, password, (int)logonType, 0, out handle);
+            return new Impersonation(domain, username, password, logonType);
+        }
+
+        private void CompleteImpersonation(bool ok, IntPtr handle)
+        {
             if (!ok)
             {
                 var errorCode = Marshal.GetLastWin32Error();
+
+                if (handle != IntPtr.Zero)
+                   NativeMethods.CloseHandle(handle);
+
                 throw new ApplicationException(string.Format("Could not impersonate the elevated user.  LogonUser returned error code {0}.", errorCode));
             }
 
             _handle = new SafeTokenHandle(handle);
             _context = WindowsIdentity.Impersonate(_handle.DangerousGetHandle());
+        }
+
+        private Impersonation(string domain, string username, SecureString password, LogonType logonType)
+        {
+            IntPtr handle;
+            var passPtr = Marshal.SecureStringToGlobalAllocUnicode(password);
+            bool ok;
+            try
+            {
+                ok = NativeMethods.LogonUser(username, domain, passPtr, (int)logonType, 0, out handle);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(passPtr);
+            }
+
+            CompleteImpersonation(ok, handle);
+        }
+
+        private Impersonation(string domain, string username, string password, LogonType logonType)
+        {
+            IntPtr handle;
+            var ok = NativeMethods.LogonUser(username, domain, password, (int)logonType, 0, out handle);
+            CompleteImpersonation(ok, handle);
         }
 
         public void Dispose()
--- SimpleImpersonation/NativeMethods.cs
+++ SimpleImpersonation/NativeMethods.cs
@@ -10,6 +10,9 @@ namespace SimpleImpersonation
         [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
         internal static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword, int dwLogonType, int dwLogonProvider, out IntPtr phToken);
 
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern bool LogonUser(String lpszUsername, String lpszDomain, IntPtr phPassword, int dwLogonType, int dwLogonProvider, out IntPtr phToken);
+
         [DllImport("kernel32.dll")]
         [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
         [SuppressUnmanagedCodeSecurity]
