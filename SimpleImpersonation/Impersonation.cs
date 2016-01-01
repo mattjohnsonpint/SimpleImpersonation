86014ec02e8a15b9b106b5121fcc0274ded8e3b3
Matt Johnson <mj1856@hotmail.com>
2015-12-31 18:24:16 -0800

    Adding xml-docs to public methods

--- SimpleImpersonation/Impersonation.cs
+++ SimpleImpersonation/Impersonation.cs
@@ -6,17 +6,40 @@ using System.Security.Principal;
 
 namespace SimpleImpersonation
 {
+    /// <summary>
+    /// Impersonates a specific user for the lifetime of this object.
+    /// </summary>
     [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
     public sealed class Impersonation : IDisposable
     {
         private readonly SafeTokenHandle _handle;
         private readonly WindowsImpersonationContext _context;
 
+        /// <summary>
+        /// Attempts to impersonate the user with the supplied information.
+        /// Call from a <c>using</c> block, or ensure that <see cref="Dispose"/> is called on
+        /// the resulting <see cref="Impersonation"/> object upon completion.
+        /// </summary>
+        /// <param name="domain">The domain name or machine name, or <c>.</c> for the local machine.</param>
+        /// <param name="username">The user name.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="logonType">The logon type.</param>
+        /// <returns>An <see cref="Impersonation"/> object, which should be disposed when done impersonating the user.</returns>
         public static Impersonation LogonUser(string domain, string username, string password, LogonType logonType)
         {
             return new Impersonation(domain, username, password, logonType);
         }
 
+        /// <summary>
+        /// Attempts to impersonate the user with the supplied information.
+        /// Call from a <c>using</c> block, or ensure that <see cref="Dispose"/> is called on
+        /// the resulting <see cref="Impersonation"/> object upon completion.
+        /// </summary>
+        /// <param name="domain">The domain name or machine name, or <c>.</c> for the local machine.</param>
+        /// <param name="username">The user name.</param>
+        /// <param name="password">The password, as a <see cref="SecureString"/>.</param>
+        /// <param name="logonType">The logon type.</param>
+        /// <returns>An <see cref="Impersonation"/> object, which should be disposed when done impersonating the user.</returns>
         public static Impersonation LogonUser(string domain, string username, SecureString password, LogonType logonType)
         {
             return new Impersonation(domain, username, password, logonType);
@@ -65,6 +88,9 @@ namespace SimpleImpersonation
             context = WindowsIdentity.Impersonate(_handle.DangerousGetHandle());
         }
 
+        /// <summary>
+        /// Disposes the <see cref="Impersonation"/> object, ending impersonation and restoring the original user.
+        /// </summary>
         public void Dispose()
         {
             _context.Dispose();
