be6ea2cb2fcb09ee6da00de9fe28622d0d591cb7
Matt Johnson <mj1856@hotmail.com>
2015-12-31 18:58:17 -0800

    Bump version and simplify

--- SimpleImpersonation/Properties/AssemblyInfo.cs
+++ SimpleImpersonation/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A tiny library that lets you impersonate any user, by acting as a managed wrapper for the LogonUser Win32 function.")]
 [assembly: AssemblyCompany("Matt Johnson")]
 [assembly: AssemblyProduct("Simple Impersonation")]
-[assembly: AssemblyCopyright("Copyright © 2013, Matt Johnson")]
+[assembly: AssemblyCopyright("Copyright © Matt Johnson")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1.2")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
