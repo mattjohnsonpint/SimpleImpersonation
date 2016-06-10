d57efd0df895b4adf26b45f4af3acc3e5407302d
Jörg B <jb@joergbattermann.com>
2016-06-10 13:38:36 +0200

    Throwing Win32Exception instead of ApplicationException

--- SimpleImpersonation/Impersonation.cs
+++ SimpleImpersonation/Impersonation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Permissions;
@@ -80,8 +81,8 @@ namespace SimpleImpersonation
                 {
                     NativeMethods.CloseHandle(token);
                 }
-
-                throw new ApplicationException(string.Format("Could not impersonate the elevated user.  LogonUser returned error code {0}.", errorCode));
+                
+                throw new Win32Exception(errorCode);
             }
 
             handle = new SafeTokenHandle(token);
