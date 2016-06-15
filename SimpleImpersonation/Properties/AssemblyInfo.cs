03e48bd941adc3cf03f66cac62944b18c8907da0
Matt Johnson <matt.johnson@microsoft.com>
2016-06-15 15:06:14 -0700

    Bump Version

--- SimpleImpersonation/Properties/AssemblyInfo.cs
+++ SimpleImpersonation/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Matt Johnson")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1.*")]
+[assembly: AssemblyInformationalVersion("2.0.1")]
