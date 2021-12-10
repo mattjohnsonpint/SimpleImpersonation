17d515101addaeb61b5a2a998552032a2554ea16
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2021-12-09 18:11:37 -0800

    Update logon provider enums

--- src/LogonProvider.cs
+++ src/LogonProvider.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleImpersonation
+﻿using System;
+
+namespace SimpleImpersonation
 {
     /// <summary>
     /// Specifies the type of login provider used.
@@ -7,14 +9,37 @@
     public enum LogonProvider
     {
         /// <summary>
-        /// Use the standard logon provider for the system.
-        /// The default security provider is negotiate, unless you pass NULL for the domain name and the user name
-        /// is not in UPN format. In this case, the default provider is NTLM.
-        /// NOTE: Windows 2000/NT:   The default security provider is NTLM.
+        /// Use the standard logon provider for the system. The default provider is "Negotiate".
+        /// However, if you pass NULL for the domain name and the user name is not in UPN format, then the default provider is "NTLM".
         /// </summary>
         Default = 0,
+
+        /// <summary>
+        /// Use the NTLM logon provider.
+        /// </summary>
+        NTLM = 2,
+
+        /// <summary>
+        /// Use the Negotiate logon provider.
+        /// </summary>
+        Negotiate = 3,
+
+        /// <summary>
+        /// Use the WINNT35 logon provider.
+        /// </summary>
+        [Obsolete("This logon provider has been deprecated and should no longer be used.")]
         WINNT35 = 1,
+
+        /// <summary>
+        /// Use the WINNT40 logon provider.
+        /// </summary>
+        [Obsolete("This enumeration value is obsolete.  To use this provider, specify LogonProvider.NTLM instead.")]
         WINNT40 = 2,
+
+        /// <summary>
+        /// Use the WINNT50 logon provider.
+        /// </summary>
+        [Obsolete("This enumeration value is obsolete.  To use this provider, specify LogonProvider.Negotiate instead.")]
         WINNT50 = 3,
     }
 }
