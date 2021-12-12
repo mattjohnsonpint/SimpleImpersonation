a97465422af0a8c4680ff4956374fd076af0e551
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2021-12-12 12:39:54 -0800

    Obsolete old methods
    Closes #74

--- src/Impersonation.cs
+++ src/Impersonation.cs
@@ -2,18 +2,12 @@
 using System.Security.Principal;
 using Microsoft.Win32.SafeHandles;
 
-#if NETFRAMEWORK
-using System.Security.Permissions;
-#endif
-
 namespace SimpleImpersonation
 {
     /// <summary>
     /// Provides ability to run code within the context of a specific user.
     /// </summary>
-#if NETFRAMEWORK
-    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
-#endif
+    [Obsolete("This class is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
     public static class Impersonation
     {
         /// <summary>
@@ -22,9 +16,10 @@ namespace SimpleImpersonation
         /// <param name="credentials">The credentials of the user account to impersonate.</param>
         /// <param name="logonType">The logon type used when impersonating the user account.</param>
         /// <param name="action">The action to perform.</param>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static void RunAsUser(UserCredentials credentials, LogonType logonType, Action action)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType))
+            using (var tokenHandle = credentials.LogonUser(logonType))
             {
                 WindowsIdentity.RunImpersonated(tokenHandle, action);
             }
@@ -37,9 +32,10 @@ namespace SimpleImpersonation
         /// <param name="logonType">The logon type used when impersonating the user account.</param>
         /// <param name="logonProvider">The logon provider used when impersonating the user account.</param>
         /// <param name="action">The action to perform.</param>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static void RunAsUser(UserCredentials credentials, LogonType logonType, LogonProvider logonProvider, Action action)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType, logonProvider))
+            using (var tokenHandle = credentials.LogonUser(logonType, logonProvider))
             {
                 WindowsIdentity.RunImpersonated(tokenHandle, action);
             }
@@ -51,9 +47,10 @@ namespace SimpleImpersonation
         /// <param name="credentials">The credentials of the user account to impersonate.</param>
         /// <param name="logonType">The logon type used when impersonating the user account.</param>
         /// <param name="action">The action to perform, which accepts a <see cref="SafeAccessTokenHandle"/> to the user account as its only parameter.</param>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static void RunAsUser(UserCredentials credentials, LogonType logonType, Action<SafeAccessTokenHandle> action)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType))
+            using (var tokenHandle = credentials.LogonUser(logonType))
             {
                 WindowsIdentity.RunImpersonated(tokenHandle, () => action(tokenHandle));
             }
@@ -66,9 +63,10 @@ namespace SimpleImpersonation
         /// <param name="logonType">The logon type used when impersonating the user account.</param>
         /// <param name="logonProvider">The logon provider used when impersonating the user account.</param>
         /// <param name="action">The action to perform, which accepts a <see cref="SafeAccessTokenHandle"/> to the user account as its only parameter.</param>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static void RunAsUser(UserCredentials credentials, LogonType logonType, LogonProvider logonProvider, Action<SafeAccessTokenHandle> action)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType, logonProvider))
+            using (var tokenHandle = credentials.LogonUser(logonType, logonProvider))
             {
                 WindowsIdentity.RunImpersonated(tokenHandle, () => action(tokenHandle));
             }
@@ -82,9 +80,10 @@ namespace SimpleImpersonation
         /// <param name="logonType">The logon type used when impersonating the user account.</param>
         /// <param name="function">The function to execute, which accepts a <see cref="SafeAccessTokenHandle"/> to the user account as its only parameter.</param>
         /// <returns>The result of executing the function.</returns>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static T RunAsUser<T>(UserCredentials credentials, LogonType logonType, Func<T> function)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType))
+            using (var tokenHandle = credentials.LogonUser(logonType))
             {
                 return WindowsIdentity.RunImpersonated(tokenHandle, function);
             }
@@ -99,9 +98,10 @@ namespace SimpleImpersonation
         /// <param name="logonProvider">The logon provider used when impersonating the user account.</param>
         /// <param name="function">The function to execute, which accepts a <see cref="SafeAccessTokenHandle"/> to the user account as its only parameter.</param>
         /// <returns>The result of executing the function.</returns>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static T RunAsUser<T>(UserCredentials credentials, LogonType logonType, LogonProvider logonProvider, Func<T> function)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType, logonProvider))
+            using (var tokenHandle = credentials.LogonUser(logonType, logonProvider))
             {
                 return WindowsIdentity.RunImpersonated(tokenHandle, function);
             }
@@ -115,9 +115,10 @@ namespace SimpleImpersonation
         /// <param name="logonType">The logon type used when impersonating the user account.</param>
         /// <param name="function">The function to execute.</param>
         /// <returns>The result of executing the function.</returns>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static T RunAsUser<T>(UserCredentials credentials, LogonType logonType, Func<SafeAccessTokenHandle, T> function)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType))
+            using (var tokenHandle = credentials.LogonUser(logonType))
             {
                 return WindowsIdentity.RunImpersonated(tokenHandle, () => function(tokenHandle));
             }
@@ -132,9 +133,10 @@ namespace SimpleImpersonation
         /// <param name="logonProvider">The logon provider used when impersonating the user account.</param>
         /// <param name="function">The function to execute.</param>
         /// <returns>The result of executing the function.</returns>
+        [Obsolete("This method is no longer needed.  Instead, use the result of UserCredentials.LogonUser with the WindowsIdentity.RunImpersontated or RunImpersonatedAsync methods.")]
         public static T RunAsUser<T>(UserCredentials credentials, LogonType logonType, LogonProvider logonProvider, Func<SafeAccessTokenHandle, T> function)
         {
-            using (var tokenHandle = credentials.Impersonate(logonType, logonProvider))
+            using (var tokenHandle = credentials.LogonUser(logonType, logonProvider))
             {
                 return WindowsIdentity.RunImpersonated(tokenHandle, () => function(tokenHandle));
             }
